1ecd3740e333095d212683e9f38a0575dc265240
MarikaKonturova <mashakonturova@gmail.com>
2023-03-22 09:27:23 +0300

    создание файлов миграции & конфигураци приложения

--- cmd/main.go
+++ cmd/main.go
@@ -7,23 +7,31 @@ import (
 	handler "github.com/MarikaKonturova/todo-go-app/pkg/handlers"
 	"github.com/MarikaKonturova/todo-go-app/pkg/repository"
 	"github.com/MarikaKonturova/todo-go-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 /* точка запуска программы */
 func main() {
-	/*
-	 *инициализируем экземпляр сервера с помощью ключевого слова new
-	 */
-	srv := new(todo.Server)
 
-	/*
-	*объявляем все зависимости в нужном порядке
-	 */
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s ", err.Error())
+	}
+	srv := new(todo.Server)
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+/*
+*создаём функцию для работы с конфигурацией
+ */
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
